09b320ef94ec181b8e7efb4e179373e15f5ab832
Eshika-Biswas <eshikabiswas801@gmail.com>
2026-07-08 11:27:09 +0600

    Fix navbar logo reference, resolve naming collision, and move Microsoft logo to partners directory

--- components/Header.tsx
+++ components/Header.tsx
@@ -84,7 +84,7 @@ export default function Header() {
           {/* Logo */}
           <Link href="/" className="flex items-center group">
             <Image
-              src="/logo.png"
+              src="/logos/essl-logo.jpeg"
               alt="ESS - Ensure Support Services"
               width={200}
               height={74}
